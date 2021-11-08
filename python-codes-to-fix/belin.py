a2f70b76fe4558c01b47ddbc915882f2f416c04f
belinipek <90098325+belinipek@users.noreply.github.com>
2021-11-08 13:09:05 +0300

    Update belin.py

--- python-codes-to-fix/belin.py
+++ python-codes-to-fix/belin.py
@@ -1,11 +1,11 @@
 def add_two(a):
-    return a + 7
+    return int(a) + 2
 
 if __name__ == '__main__':
     print("Give the number to be added by two.")
     a = input()
     result = add_two(a)
-    if(result == a+2):
+    if(result == int(a) +2):
         print("You fixed it.")
     else:
         print("Result is: ", result, ". Try again.")
